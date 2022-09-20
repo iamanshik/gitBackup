0b85535cfb38304be0a8ff0fd830b9715c142a79
anshiq <anshikthind@gmail.com>
2022-09-20 21:23:45 +0530

    yt web

--- yt type web/react portion/src/components/signInUp/SignIn.js
+++ yt type web/react portion/src/components/signInUp/SignIn.js
@@ -1,43 +1,57 @@
 import React from "react";
 import "./sign.css";
 export default function SignIn() {
+  let response, jsonData;
   const [formData, setFormData] = React.useState({
     email: "",
     password: "",
   });
 
   function handleChange(event) {
-    const { name, value } = event.target; // all properties we have to work with each element
+    const { name, value } = event.target;
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
-    })); // this time returning whole thing as an object.
+    }));
   }
-  function loginDataBase(){
-    console.log(formData.password,formData.email)
-      fetch(`http://localhost/login/anshikthind@yahoo.com`)
-      .then((res)=>res.json())
-      .then((data)=>console.log(data))
+
+  function loginDataBase() {
+    // console.log(formData.password,formData.email)
+    fetch(`http://localhost/login/${formData.email}`)
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else if (res.status === 404) {
+          alert("invalid Creadentials");
+          return false;
+        } else if (res.status === 500) {
+          alert("serverside problem");
+          return false;
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        if (data) {
+          return 'fdddd';
+        } else {
+          setFormData(data);
+        }
+      });
+    // console.log(formData)
   }
-  console.log(formData.password.length);
+  // console.log(formData.password.length);
   function handleSubmit(event) {
     event.preventDefault(); // to dont refresh the page. and also to not paste credentials in url.
-    if ( formData.email.length === 0) {
+    if (formData.email.length === 0) {
       alert("please Provide Credentials");
-    }
-    else{ 
-
-      if (formData.password.length < 8)
-      {
+    } else {
+      if (formData.password.length < 8) {
         alert("password must be eigth laters");
-      }
-      else{
+      } else {
         // alert('you  are loggged in')
         loginDataBase();
       }
-      
     }
-
   }
   return (
     <>
